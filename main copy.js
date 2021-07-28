d53a3c8cb4b0ba2a7d03e761f1637cae4c2fcaee
ZoePorta <zoeportagarcia@gmail.com>
2021-07-28 19:19:57 +0200

    Posible añadir varias fechas, se guardan en localStorage

---
+++ main copy.js
@@ -0,0 +1,18 @@
+import { intervalToDuration as distance, formatDuration } from "date-fns";
+import { es } from "date-fns/locale";
+
+function uploadTimer() {
+  let interval = distance({
+    start: new Date(),
+    end: new Date("06 Aug 2021 15:00:00 GMT+1"),
+  });
+
+  const intervalString = formatDuration(interval, { locale: es });
+  const titulo = document.querySelector("h1");
+
+  titulo.textContent = "Quedan " + intervalString;
+}
+
+setInterval(uploadTimer, 1000);
+
+uploadTimer();
--- main.js
+++ main.js
@@ -1,16 +1,76 @@
-import { intervalToDuration as distance, formatDuration } from 'date-fns';
-import {es} from "date-fns/locale"
+import {
+  intervalToDuration as distance,
+  formatDuration,
+  isBefore,
+  parseISO,
+} from "date-fns";
+import { es } from "date-fns/locale";
 
+const form = document.forms[0];
+const lista = document.querySelector("ul");
+const clean = document.querySelector("#clean");
 
-function uploadTimer(){
-let interval = distance({start: new Date(), end:new Date("06 Aug 2021 15:00:00 GMT+1")}/* , {locale: es, addSuffix: true} */)
+function cleanHandler(e) {
+  e.preventDefault();
+  dates = dates.filter((date) => isBefore(new Date(), parseISO(date.date)));
 
-const intervalString = formatDuration(interval, {locale: es})
-const titulo = document.querySelector("h1")
-
-titulo.textContent = "Quedan " + intervalString
+  localStorage.setItem("dates", JSON.stringify(dates));
+  uploadTimer();
 }
 
-setInterval(uploadTimer, 1000)
+let dates = [];
 
-uploadTimer()
+const storagedDates = JSON.parse(localStorage.getItem("dates"));
+
+if (storagedDates) {
+  dates = storagedDates;
+}
+
+clean.addEventListener("click", cleanHandler);
+
+function newDateHandler(e) {
+  e.preventDefault();
+  let { date, name } = form;
+  date = date.value;
+  name = name.value;
+  if (!date || !name) {
+    alert("Debes introducir un numbre y una fecha válidos");
+    return;
+  }
+
+  dates.push({ name, date });
+
+  localStorage.setItem("dates", JSON.stringify(dates));
+  uploadTimer();
+}
+
+form.addEventListener("submit", newDateHandler);
+
+function uploadTimer() {
+  lista.innerHTML = "";
+  const fragment = document.createDocumentFragment();
+  for (const date of dates) {
+    const newDate = document.createElement("li");
+    newDate.textContent = getDateString(date);
+    fragment.append(newDate);
+  }
+  lista.append(fragment);
+}
+
+function getDateString({ date, name }) {
+  if (isBefore(parseISO(date), new Date())) {
+    return `No queda nada para ${name}`;
+  }
+  let interval = distance({
+    start: new Date(),
+    end: new Date(date),
+  });
+
+  const intervalString = formatDuration(interval, { locale: es });
+
+  return `Quedan ${intervalString} para ${name}`;
+}
+
+setInterval(uploadTimer, 1000);
+
+uploadTimer();
